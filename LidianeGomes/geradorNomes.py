0cfef88522eb6be2001d4af40626478853f3015d
Lidiane M.  Gomes <lidigomes@gmail.com>
2024-06-10 17:04:36 -0300

    Criação do programa e do Readme

---
+++ LidianeGomes/geradorNomes.py
@@ -0,0 +1,8 @@
+print('Bem-vindo ao gerador de nomes divertidos!')
+
+cor = input('Digite uma cor: ').capitalize()
+animal = input('Digite um animal: ').capitalize()
+danca = input('Digite uma dança: ').capitalize()
+sobremesa = input('Digite uma sobremesa: ').capitalize()
+
+print('Prazer em te conhecer', animal, sobremesa,'de', danca, cor,'!')
