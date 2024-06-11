ed8f1a4366fa2dc1f66a298f123ffdfdaf33dced
Ana Beatriz Figueiredo <anabiafigue01@gmail.com>
2024-06-11 18:54:48 -0400

    exercicio

---
+++ Ana_Beatriz/gerador_nomes.py
@@ -0,0 +1,4 @@
+nom1 = input('digite o nome do seu sabor de sorvete favorito: ')
+nom2 = input('digite o seu personagem de desenho animado favorito: ')
+nom3 = input('digite um gênero musical que você menos gosta: ')
+print('Seu nome divertido é ' + nom1,nom2,nom3)
