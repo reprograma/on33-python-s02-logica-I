2eff6c06b5287e5eab50c175c5ecbb3f3d953bfb
beamrls <biamota49@gmail.com>
2024-06-10 21:51:41 -0300

    Adicionando atividade de gerador de nomes divertidos

--- gerador_nomes.py
+++ gerador_nomes.py
@@ -1,5 +1,7 @@
 Cor = input('Qual sua cor favorita? ')
+
 Animal = input('Qual animal te representa mais? ')
+
 Sobremesa = input('Qual sua sobremesa favorita? ')
 
 print(f'Seu nome divertido é {Animal} {Cor} {Sobremesa}!')
