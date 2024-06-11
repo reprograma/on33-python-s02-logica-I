ee7487acbe3a1f0691105edb1c00fd748e31d05b
Andreza Taina <andrezatainaaa@gmail.com>
2024-06-11 11:29:37 -0300

    exercicio da semana 02

---
+++ exercicios/para-casa/andreza_taina/gerador_nomes.py
@@ -0,0 +1,8 @@
+nome = input('Qual é o nome do seu animal?')
+cor =  input('Qual é a cor dele?')
+comida = input('Ele te lembra qual comida?')
+artista = input('Com qual artista ele se parece?:')
+
+print('\n Esse é o nome do seu cachorro:') 
+
+
