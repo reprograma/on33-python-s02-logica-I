9979eef628642ce9a8144c1b3a23dbf4d65237dc
Erika Roanna <erikaroanna@alu.ufc.br>
2024-06-12 20:00:46 -0300

    Adicionando atividade de gerador de nomes divertidos

---
+++ Roanna/gerador_nomes.py
@@ -0,0 +1,8 @@
+print('Hello Reprograma')
+nome1 = input('sua cor favorita')
+nome2 =input('seu animal favotito')
+nome3 =input('sua sobremesa favorita')
+nome4 =input('seu filme favorito')
+
+print('\nseu nome divertido é:')
+print(nome2,nome3,nome1)
