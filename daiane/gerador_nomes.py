d7a9541d2218bbe542e0fbbcfb6f3baba017ce7f
daianefernandes8 <daianesantosfernandes8@gmail.com>
2024-06-12 20:46:59 -0300

    exercicio semana2

--- daiane/gerador_nomes.py
+++ daiane/gerador_nomes.py
@@ -1,5 +1,11 @@
-nome1=input('sua flor favorita:')
-nome2=input('sua cor favorita:')
-nome3=input('vc grita ou chama:')
-nome4=input('palavra de alerta:')
- print('seu nome divertido é' +nome1,nome2,nome3,nome4)
+print('bem vindo')
+nome1 = input('sua flor favorita:')
+nome2 = input('sua cor favorita:')
+nome3 = input('vc grita ou chama:')
+nome4 = input('palavra de alerta:')
+
+nome_divertido =f'seu nome divertido é:{nome1} {nome2} {nome3} {nome4}'
+
+
+print(nome_divertido)
+ 
