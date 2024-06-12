2e2e9f85bb0ebc5849ef265a70da388356fe6d41
ClaireBP <claire.pretinha@hotmail.com>
2024-06-12 09:50:54 -0300

    exercíco semana 2

---
+++ CLAÍRE/gerador_nomes.py
@@ -0,0 +1,4 @@
+usuario_cor = input("Digite uma cor: ")
+usuario_animal = input("Digite um animal: ")
+usuario_sobremesa = input("Digite uma sobremesa: ")
+print(f"Meu mundo é {usuario_cor}, Meu diverto é {usuario_animal} e minha terapia é o {usuario_sobremesa}")
