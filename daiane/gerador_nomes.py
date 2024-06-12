93543c5d7f1c7755f7ed9eb2d6c00c1d80854ebe
daianefernandes8 <daianesantosfernandes8@gmail.com>
2024-06-12 19:03:36 -0300

    gerador de frase divertida

---
+++ daiane/gerador_nomes.py
@@ -0,0 +1,5 @@
+nome1=input('sua flor favorita:')
+nome2=input('sua cor favorita:')
+nome3=input('vc grita ou chama:')
+nome4=input('palavra de alerta:')
+ print('seu nome divertido é' +nome1,nome2,nome3,nome4)
