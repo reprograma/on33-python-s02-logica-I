828901a0527c0e39b466a7b54d302de6ddb0f12a
Ladiane P.S <aladydyljc@gmail.com>
2024-06-12 20:19:25 -0300

    Adicionar atividade de nomes divertidos

---
+++ Ladiane/gerador_nomes.py
@@ -0,0 +1,8 @@
+print("\n" + "#" * 40)
+print("Bem-vindo ao Gerador de Nomes Divertidos!")
+print("#" * 40)
+fruta = input("Por favor, insira uma fruta: ")
+verbo = input("Agora, insira um verbo: ")
+objeto = input("Por fim, insira uma objeto: ")
+
+print(f"Seu nome divertido é {fruta} {verbo} {objeto}")
