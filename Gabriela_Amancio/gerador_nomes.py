dbf67fb6eeae6cf38a29171009f5f85578a34d7e
GabyAmancio <gabrielaamancio001@gmail.com>
2024-06-19 22:41:59 -0300

    Adicionando atividade de gerador de nomes divertidos~

---
+++ Gabriela_Amancio/gerador_nomes.py
@@ -0,0 +1,7 @@
+cor = input('Digite uma cor: ')
+animal = input('Digite um animal: ')
+sobremesa = input('Digite uma sobremesa: ')
+nome = (cor + ' ' + animal+ ' ' + sobremesa)
+
+print('\n Seu nome divertido é: ' + nome)
+
